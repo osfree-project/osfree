861affa13ec15cb27489fa6d323da41bb4178942
valerius2k <_valerius@mail.ru>
2007-09-08 09:21:19 +0000

    Fixed uXFD.h and added uXFD.inc

--- include/uXFD/uXFD.h
+++ include/uXFD/uXFD.h
@@ -1,1 +1,23 @@
-int load(char far *image, unsigned long size, char far *load_addr, struct exe_params *p);
+/*
+ *   Load executable image
+ *
+ *   \param   image      -- executable image phys addrerss
+ *   \param   size       -- its size
+ *   \param   load_addr  -- address to load.
+ *                          must be zero if load at linked-in address
+ *   \retval  p          -- structure with loaded image properties,
+ *                          such as cs:[e]ip, ss:[e]sp etc.
+ *   \return  0          -- if loaded successfully,
+ *                          other value if error
+ */
+
+//int load(char far *image, unsigned long size, char far *load_addr, struct exe_params *p);
+int load(unsigned long image, unsigned long size, unsigned long load_addr, struct exe_params *p);
+
+struct exe_params
+{
+   unsigned short  cs;
+   unsigned long   eip;
+   unsigned short  ss;
+   unsigned long   esp;
+}
