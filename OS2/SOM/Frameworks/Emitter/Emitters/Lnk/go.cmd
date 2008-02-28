13a9a6d848fd0f1f8347a5d4895c800e3d67c2a2
prokushev <yuriprokushev@gmail.com>
2008-02-28 14:46:50 +0000

    Watcom linker emitter added to build tree

--- OS2/SOM/Frameworks/Emitter/Emitters/Lnk/go.cmd
+++
@@ -1,6 +0,0 @@
-set path=c:\_work\svn\emitpas\compiler
-set dpath=c:\_work\svn\emitpas\compiler
-set beginlibpath=c:\_work\svn\emitpas\compiler
-set include=.;c:\_work\svn\emitpas\compiler
-set watcom=c:\_work\svn\emitpas\compiler
-make.cmd
