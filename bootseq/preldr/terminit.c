a9252b829f7fcbbdd8fa32375a99e52824162146
valerius2k <_valerius@mail.ru>
2008-04-14 21:05:02 +0000

    Added missing files

---
+++ bootseq/preldr/terminit.c
@@ -0,0 +1,15 @@
+/*  terminal blackbox init
+ *  (setting lip1)
+ */
+ 
+#include <lip.h>
+
+void init(lip1_t *l)
+{
+
+}
+
+void cmain(void)
+{
+
+}
---
+++ bootseq/preldr/termstrt.asm
@@ -0,0 +1,49 @@
+;
+; termstrt.asm:
+; terminal blackbox (uT) segments
+; alignment and ordering
+;
+
+name termstart
+
+include fsd.inc
+
+.386p
+
+; Start of the executable
+BASE1      equ TERM_BASE - 10000h
+; Start of 32-bit part
+BASE2	   equ BASE1 + 380h
+
+_TEXT16  segment dword public 'CODE'  use16
+	   org 0h
+header_begin:
+	   ; here we simulate 32-bit call instruction
+	   ; WASM can't generate 32-bit instruction
+	   ; inside 16-bit segment, so this workaround.
+	   db	0e8h
+	   dd	offset _TEXT:start2 - TERM_BASE - 5
+	   ; a sort of header signature
+	   db	'$header$'
+	   ; padding with NOP's
+	   db	20h - ($ - header_begin)	dup (90h)
+header_end:
+_TEXT16  ends
+_TEXT    segment dword public 'CODE'  use32
+	   org BASE1
+start1:
+	   org BASE2
+start2:
+_TEXT    ends
+_DATA    segment dword public 'DATA'  use32
+_DATA    ends
+CONST    segment dword public 'DATA'  use32
+CONST    ends
+CONST2   segment dword public 'DATA'  use32
+CONST2   ends
+_BSS     segment dword public 'BSS'   use32
+_BSS     ends
+
+DGROUP   group _TEXT,_DATA,CONST,CONST2,_BSS
+
+         end
