41f83f7c69a2842e50fe133f3b6850dbb13c700f
valerius2k <_valerius@mail.ru>
2008-04-20 10:27:06 +0000

    Enhanced new mkiso.cmd script

--- bootseq/preldr/mkiso.cmd
+++ bootseq/preldr/mkiso.cmd
@@ -1,54 +1,63 @@
 @echo off
 
-rem
-rem make bootable ISO image
-rem with FreeLdr
-rem
+rem .......................... 
+rem (c) osFree project, 2008 .
+rem make bootable ISO image  .
+rem with FreeLdr             .
+rem ..........................
 
+@echo Creating bootable iso image...
 
-if   not exist preldr0 wmake preldr0
-if   not exist iso9660.fsd wmake iso9660.fsd
-cd   ..\bootsec\eltorito
-if   not exist eltorito.bin wmake eltorito.bin
-cd   ..\..\preldr
-copy ..\bootsec\eltorito\eltorito.bin .
-call mkboot.cmd eltorito.bin preldr0 iso9660.fsd bootblk
+rem Current working directory
+set cwd=%ROOT%\bootseq\preldr
 
-if not exist ..\..\..\cd mkdir ..\..\..\cd
-if not exist ..\..\..\cd\boot mkdir ..\..\..\cd\boot
-if not exist ..\..\..\cd\boot\freeldr mkdir ..\..\..\cd\boot\freeldr
-if not exist ..\..\..\cd\boot\freeldr\fsd mkdir ..\..\..\cd\boot\freeldr\fsd
-if not exist ..\..\..\cd\boot\freeldr\term mkdir ..\..\..\cd\boot\freeldr\term
-if not exist ..\..\..\cd\l4 mkdir ..\..\..\cd\l4
-if not exist ..\..\..\cd\pns mkdir ..\..\..\cd\pns
-if not exist ..\..\..\cd\os2 mkdir ..\..\..\cd\os2
+set dir1=%root%\bootseq\preldr
+set files1=preldr0 iso9660.fsd
 
-move bootblk ..\..\..\cd\boot
-copy preldr0 ..\..\..\cd\boot\freeldr
-copy preldr.ini  ..\..\..\cd\boot\freeldr
-copy freeldr.cfg ..\..\..\cd\boot\freeldr
-copy *.fsd   ..\..\..\cd\boot\freeldr\fsd
-copy *.trm   ..\..\..\cd\boot\freeldr\term
-copy *.rel   ..\..\..\cd\boot\freeldr\fsd
-move ..\..\..\cd\boot\freeldr\fsd\preldr0.rel ..\..\..\cd\boot\freeldr
-move ..\..\..\cd\boot\freeldr\fsd\serial.rel ..\..\..\cd\boot\freeldr\term\
-move ..\..\..\cd\boot\freeldr\fsd\hercules.rel ..\..\..\cd\boot\freeldr\term\
-move ..\..\..\cd\boot\freeldr\fsd\console.rel ..\..\..\cd\boot\freeldr\term\
-copy freeldr  ..\..\..\cd\boot\freeldr
-copy boot_lin ..\..\..\cd\boot\freeldr
-copy boot_chn ..\..\..\cd\boot\freeldr
+set dir2=%root%\bootseq\bootsec\eltorito
+set files2=eltorito.bin
+
+@for %%l in (1 2) do ^
+  (set f=%%files%%l%% && ^
+   set d=%%dir%%l%%   && ^
+   (@for %%i in (%f%) do ^
+     (cd %d% && (@wmake %%i) && cd %cwd%)))
+
+@call mkboot.cmd eltorito.bin preldr0 iso9660.fsd bootblk
+
+cd ..\..\..
+set dirs=cd cd\boot cd\boot\freeldr cd\boot\freeldr\fsd cd\boot\freeldr\term cd\l4 cd\pns cd\os2 cd\l4ka
+@for %%i in (%dirs%) do if not exist %%i mkdir %%i
+cd osfree\bootseq\preldr
+
+@move bootblk ..\..\..\cd\boot
+set files=preldr0 preldr.ini freeldr freeldr.cfg boot_lin boot_chn boot_dsk boot
+@for %%i in (%files%) do if exist %%i copy %%i  ..\..\..\cd\boot\freeldr
+
+@copy *.fsd   ..\..\..\cd\boot\freeldr\fsd
+@copy *.trm   ..\..\..\cd\boot\freeldr\term
+@copy *.rel   ..\..\..\cd\boot\freeldr\fsd
+
+@move ..\..\..\cd\boot\freeldr\fsd\preldr0.rel ..\..\..\cd\boot\freeldr
+set files=serial.rel hercules.rel console.rel
+for %%i in (%files%) do if exist %%i move ..\..\..\cd\boot\freeldr\fsd\%%i ..\..\..\cd\boot\freeldr\term
+
 cd ..\..\fiasco
-copy * ..\..\cd\l4
+@copy * ..\..\cd\l4
 cd ..\pns
-copy * ..\..\cd\pns
+@copy * ..\..\cd\pns
+cd ..\files
+@copy * ..\..\cd\l4ka
+
 cd ..\os2\server
-copy os2server ..\..\..\cd\os2
-copy VioWrtTTY_test ..\..\..\cd\os2
-copy config.sys ..\..\..\cd\os2
-copy libkal.s.so ..\..\..\cd\os2
-copy os2.cfg ..\..\..\cd\os2
-copy MiniELFExe.Exe ..\..\..\cd\os2
+set files=os2server VioWrtTTY_test config.sys libkal.s.so os2.cfg MiniELFExe.Exe
+@for %%i in (%files%) do if exist %%i copy %%i ..\..\..\cd\os2\
 cd   ..\..\..
-.\mkisofs2 -b boot/bootblk -c boot/bootcat.bin -no-emul-boot -boot-load-size 12 -boot-info-table -iso-level 3 -allow-lowercase -no-iso-translate -r -J -publisher "osFree (www.osfree.org)" -o osfree.iso cd
-move osfree.iso \data\vm\img
+
+@.\mkisofs2 -b boot/bootblk -c boot/bootcat.bin -no-emul-boot -boot-load-size 12 -boot-info-table -iso-level 3 -allow-lowercase -no-iso-translate -r -J -publisher "osFree (www.osfree.org)" -o osfree.iso cd
+@move osfree.iso \data\vm\img
 cd   osfree\bootseq\preldr
+
+goto end
+
+:end
