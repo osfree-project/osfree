596a9d25bce4d40c744f23dab705fee8f82649c0
valerius2k <_valerius@mail.ru>
2008-08-14 12:23:25 +0000

    build system update

--- mk/tools.mk
+++ mk/tools.mk
@@ -4,7 +4,6 @@
 #
 
 32_BITS  = 1
-ADD_COPT = -d__WATCOM__ -d__OS2__ -i=. -i=..$(SEP)..$(SEP)include
 
 !ifndef DEST
 DEST    = tools
@@ -14,6 +13,7 @@ DEST    = tools
 
 CLEANMASK = *.lnk *.wmp *.obj *.err *.log *.bak *.sym
 ADD_LINKOPT = $(ADD_LINKOPT) lib z,libmmap libpath $(FILESDIR)$(SEP)..$(SEP)build$(SEP)lib
+ADD_COPT = -i=. -i=..$(SEP)..$(SEP)include
 TARGETS  = $(PATH)$(PROJ).exe # $(PATH)$(PROJ).sym
 
 $(PATH)$(PROJ).exe: $(PATH)$(PROJ).lnk
