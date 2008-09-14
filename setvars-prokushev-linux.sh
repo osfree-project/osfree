06f87a86d997d4b78031173cef0621fd189b61ed
prokushev <yuriprokushev@gmail.com>
2008-09-14 10:09:04 +0000

    attempts for linux host

---
+++ setvars-prokushev-linux.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/rexx
+/**/
+parse source . . src
+p = lastpos('/', translate(src, '/', '\'))
+path = delstr(src, p)
+basename = substr(src, p + 1)
+p = lastpos('.', basename)
+if p > 0 then basename = delstr(basename, p)
+user = substr(basename, pos('-', basename) + 1)
+curdir = directory()
+call directory path
+call setenv user || '.conf'
+call directory curdir
+
+say 'New shell started'
+interpret '"'||address()||'"'
