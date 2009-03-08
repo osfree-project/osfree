50bb342c12a3e6b40a614321fca9a38c9a0ea3c2
valerius2k <_valerius@mail.ru>
2009-03-08 18:59:02 +0000

    renewed libvorbis and created makefiles for libflac, libogg and libvorbis. But libvorbis still don't work

---
+++ OS2/Shared/libs/libogg/include/ogg/config_types.h
@@ -0,0 +1,16 @@
+#ifndef __CONFIG_TYPES_H__
+#define __CONFIG_TYPES_H__
+
+/* added by valerius */
+typedef short int16_t;
+//typedef long  int32_t;
+typedef unsigned long u_int32_t;
+//typedef unsigned long long int64_t;
+
+/* these are filled in by configure */
+typedef int16_t ogg_int16_t;
+typedef int32_t ogg_int32_t;
+typedef u_int32_t ogg_uint32_t;
+typedef int64_t ogg_int64_t;
+
+#endif
