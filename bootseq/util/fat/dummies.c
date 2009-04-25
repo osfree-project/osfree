d1b439b44a9bd5da8fdc4d2acd513f22977b1170
valerius2k <_valerius@mail.ru>
2009-04-25 05:39:52 +0000

    a stub for ufat.dll

---
+++ bootseq/util/fat/dummies.c
@@ -0,0 +1,27 @@
+/*
+ *
+ *
+ */
+
+#define  INCL_OS2DEFS
+#include <os2.h>
+#include <stdio.h>
+
+
+APIRET APIENTRY CHKDSK(LONG argc, char **argv, char **envp) {
+    fprintf(stderr, "CHKDSK not implemented yet.\n");
+
+    return 0;
+}
+
+APIRET APIENTRY FORMAT(LONG argc, char **argv, char **envp) {
+    fprintf(stderr, "FORMAT not implemented yet.\n");
+
+    return 0;
+}
+
+APIRET APIENTRY RECOVER(LONG argc, char **argv, char **envp) {
+    fprintf(stderr, "RECOVER not implemented yet.\n");
+
+    return 0;
+}
---
+++ bootseq/util/fat/sys.c
@@ -0,0 +1,14 @@
+/*  Utility Filesystem funclions
+ *  
+ *
+ */
+
+#define  INCL_OS2DEFS
+#include <os2.h>
+#include <stdio.h>
+
+APIRET APIENTRY SYS(LONG argc, char **argv, char **envp) {
+    
+
+    return 0;
+}
