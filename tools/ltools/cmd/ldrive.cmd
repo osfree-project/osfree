17c32ad27db3c356c4831901cb202117dae8a645
valerius2k <_valerius@mail.ru>
2009-08-25 10:27:15 +0000

    changes flush

--- tools/ltools/cmd/ldrive.cmd
+++
@@ -1,5 +0,0 @@
-@REM set Linux default drive for LDIR, LREAD AND LWRITE to command line parameter %1
-@if ""%1=="" goto show
-@set LDRIVE=%1
-:show
-@echo Linux default drive is   %LDRIVE%
--- tools/ltools/porting.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <io.h>
-
-#define popen _popen
-#define pclose _pclose
-#define lseek64 _lseeki64
-
-void test_endianness();
