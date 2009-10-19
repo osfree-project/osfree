eae78a05526f1ddf8fcefa80a062bb8b160929ff
prokushev <yuriprokushev@gmail.com>
2009-10-19 16:28:32 +0000

    WinDlgBox implementation

---
+++ OS2/PM/API/PMMERGE/dialogs/windlgbox.c
@@ -0,0 +1,11 @@
+#include <os2.h>
+
+ULONG APIENTRY WinDlgBox(const HWND hwndParent, const HWND hwndOwner, const PFNWP pfnDlgProc, const HMODULE hmod, const ULONG idDlg, const PVOID pCreateParams)
+{
+  HWND hwndDlg = WinLoadDlg(hwndParent, hwndOwner,
+                            pfnDlgProc, hmod, idDlg, pCreateParams);
+
+  ULONG ulReply = WinProcessDlg(hwndDlg);
+
+  WinDestroyWindow(hwnd);
+}
