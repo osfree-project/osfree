3c31ebce188ef0771884daa5d8bb7eddff47d633
prokushev <yuriprokushev@gmail.com>
2010-04-28 09:54:29 +0000

    cfgsys editor dll stub

---
+++ OS2/MM/minstall/cfgsys/simples.h
@@ -0,0 +1,28 @@
+
+/*
+ *@@sourcefile simples.h:
+ *      some basic typical OS/2 definitions that are used in
+ *      some helpers include files that should also work
+ *      when os2.h is not included.
+ *
+ *      Note: Version numbering in this file relates to XWorkplace version
+ *            numbering.
+ *
+ */
+
+/*      Copyright (C) 2002 Ulrich M”ller.
+ *      This file is part of the "XWorkplace helpers" source package.
+ *      This is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published
+ *      by the Free Software Foundation, in version 2 as it comes in the
+ *      "COPYING" file of the XWorkplace main distribution.
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ */
+
+#include <os2.h>
+
+
+
