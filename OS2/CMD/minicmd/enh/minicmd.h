4c32d1fa84a2386ffaa67846192382086cd50d4a
valerius2k <_valerius@mail.ru>
2011-05-03 10:30:00 +0000

    Separated 'freezed' minimal minicmd version and enhanced one

---
+++ OS2/CMD/minicmd/enh/minicmd.h
@@ -0,0 +1,26 @@
+#define INCL_DOSPROCESS       /* Process and thread values */
+#define INCL_DOS
+#define INCL_DOSERRORS        /* DOS error values          */
+#define INCL_DOSMISC          /* DOS error values          */
+#define INCL_DOSFILEMGR
+#define INCL_BASE
+#define INCL_BSESUB
+#define INCL_KBD
+#define INCL_VIO
+#include <os2.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+// array used for internal commands (indirect function calls)
+typedef struct
+{
+        char *cmdname;                  // command name
+        int (* func)(int, char **);     // pointer to function
+} BUILTIN;
+
+int cmds_cmd(int argc, char **argv);
+int exit_cmd(int argc, char **argv);
+int chdir_cmd(int argc, char **argv);
+
+extern BUILTIN commands[];
