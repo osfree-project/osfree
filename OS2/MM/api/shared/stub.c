7fbc74d42bf78e312b9b2922fbfd2ed38c43b9ec
valerius2k <_valerius@mail.ru>
2012-02-20 06:30:28 +0000

    Added MMOS/2 forwarder DLL's

---
+++ OS2/MM/api/shared/stub.c
@@ -0,0 +1,12 @@
+/*!
+
+   @file stub.c
+
+   @brief empty file - used for forwarder dlls
+
+   (c) osFree Project 2003, <http://www.osFree.org>
+   for licence see 'legal' in this directory
+
+   @author Yuri Prokushev (prokushev@freemail.ru)
+
+*/
