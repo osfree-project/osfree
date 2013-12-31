17d4ef32865a5b2bfe6afea10e3d15491eefe8a1
valerius2k <_valerius@mail.ru>
2013-12-31 03:02:20 +0000

    Added two files I forgot to add.

---
+++ mk/appsos2_cmd.mk
@@ -0,0 +1,6 @@
+!ifndef __apps_os2_cmd__
+!define __apps_os2_cmd__
+
+!include $(%ROOT)/mk/appsos2.mk
+
+!endif
---
+++ mk/appsos2_pm.mk
@@ -0,0 +1,28 @@
+!ifndef __apps_os2_pm__
+!define __apps_os2_pm__
+
+!include $(%ROOT)/mk/appsos2_cmd.mk
+
+!ifeq UNI2H 1
+
+
+RCOPT = &
+                      -i=$(%ROOT)$(SEP)build$(SEP)include &
+                      -i=$(%ROOT)$(SEP)build$(SEP)include$(SEP)os2 &
+                      -i=$(%ROOT)$(SEP)build$(SEP)include$(SEP)shared
+
+ADD_COPT   =          $(ADD_COPT) &
+                      -i=$(%ROOT)$(SEP)include$(SEP)os3$(SEP)pm
+
+!else
+
+RCOPT = &
+                      -i=$(%WATCOM)$(SEP)h &
+                      -i=$(%WATCOM)$(SEP)h$(SEP)os2
+
+ADD_COPT   =          $(ADD_COPT) &
+                      -i=$(%ROOT)$(SEP)include$(SEP)os3$(SEP)pm
+
+!endif
+
+!endif
