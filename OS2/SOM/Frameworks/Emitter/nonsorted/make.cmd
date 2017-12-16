a34de2e908f81c759bac03319aa49846a0774f42
prokushev <yuriprokushev@gmail.com>
2017-12-16 08:47:24 +0000

    build paths fix

--- OS2/SOM/Frameworks/Emitter/nonsorted/make.cmd
+++ OS2/SOM/Frameworks/Emitter/nonsorted/make.cmd
@@ -6,7 +6,7 @@ cl /c /DWIN32_LEAN_AND_MEAN /D_WIN32 /DHAVE_CONFIG_HPP /MD /I. rhbseh2.c
 lib rhbseh2.obj -OUT:rhbseh2.lib
 cl /D_USE_SOME_ /DWIN32_LEAN_AND_MEAN /D_WIN32 /DHAVE_CONFIG_HPP /MD /I. rhbsc.cpp rhbidl.cpp rhbscapi.cpp rhbscemt.cpp rhbschdr.cpp rhbscsome.cpp rhbscir.cpp rhbsckih.cpp rhbscpp.cpp rhbsctc.cpp rhbsctyp.cpp ..\lib\somtk.lib some.lib
 
-cl /D_USE_SOME_ /DWIN32_LEAN_AND_MEAN /D_WIN32 /DHAVE_CONFIG_HPP /MD emitlib.cpp scentry.cpp scmeta.cpp sctdef.cpp scstruct.cpp scenum.cpp scunion.cpp scenumnm.cpp scclass.cpp scconst.cpp scbase.cpp scattrib.cpp sccommon.cpp scpass.cpp scdata.cpp scmodule.cpp scemit.cpp somstrt.cpp sctmplt.cpp scmethod.cpp def.cpp /LD /Fesome.dll some.def ..\lib\somtk.lib
+cl /D_USE_SOME_ /DWIN32_LEAN_AND_MEAN /D_WIN32 /DHAVE_CONFIG_HPP /MD /I. emitlib.cpp scentry.cpp scmeta.cpp sctdef.cpp scstruct.cpp scenum.cpp scunion.cpp scenumnm.cpp scclass.cpp scconst.cpp scbase.cpp scattrib.cpp sccommon.cpp scpass.cpp scdata.cpp scmodule.cpp scemit.cpp somstrt.cpp sctmplt.cpp scmethod.cpp def.cpp /LD /Fesome.dll some.def ..\lib\somtk.lib
 
 rhbsc /esome test.idl 
 
