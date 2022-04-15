62cb0d40de871988e00c80f8bbab7b432744ffef
Valery Sedletski <_valerius@mail.ru>
2022-04-15 13:42:07 +0300

    Fix linux build #10

--- _wcc.sh
+++ _wcc.sh
@@ -2,4 +2,5 @@
 #
 
 PATH=.:$PATH
+echo "PATH=\"$PATH\""
 build-lnx.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
--- build-lnx.sh
+++ build-lnx.sh
@@ -5,4 +5,7 @@
 # conf=docker.conf
 #`which rexx` setenv.cmd ${conf} >/dev/null 2>&1
 . setvars-lnx.sh
+for var in $VARS; do
+    echo "$var=\"$($var)\""
+done
 wmake -h $1 $2 $3 $4 $5 $6 $7 $8 $9
