3fb19dc607671d7ad457f1e882dddc03111e1892
Valery Sedletski <_valerius@mail.ru>
2022-04-15 15:31:14 +0300

    Fix linux build #21

--- build-lnx.sh
+++ build-lnx.sh
@@ -4,10 +4,5 @@
 # Edit here to use your own *.conf
 # conf=docker.conf
 #`which rexx` setenv.cmd ${conf} >/dev/null 2>&1
-. ./setvars-lnx.sh
-echo "VARS=$VARS"
-echo "OS=$OS"
-echo "SHELL=$SHELL"
-echo "OS_SHELL=$OS_SHELL"
-echo "REXX=$REXX"
+. setvars-lnx.sh
 wmake -h $1 $2 $3 $4 $5 $6 $7 $8 $9
