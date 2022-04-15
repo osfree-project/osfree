5005fac3f4d67628bcd945408db65e6ca31e9259
Valery Sedletski <_valerius@mail.ru>
2022-04-15 09:30:34 +0300

    Fix linux build

--- _setup.sh
+++ _setup.sh
@@ -6,22 +6,22 @@
 #
 
 ME=`whoami`
-sudo mkdir -p /root/osfree /root/dist /root/src
+mkdir -p /root/osfree /root/dist /root/src
 #echo "deb http://deb.debian.org/debian bullseye main contrib non-free" >/etc/apt/sources.list
 #echo "deb http://deb.debian.org/debian bullseye-updates main contrib non-free" >>/etc/apt/sources.list
 #echo "deb http://security.debian.org/debian-security bullseye-security main contrib non-free" >>/etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install -y wget curl unzip tar gzip bash build-essential fpc
+apt-get update
+apt-get install -y wget curl unzip tar gzip bash build-essential fpc
 cd /root/dist
 wget ftp://osfree.org/upload/ow/ow-1.9.zip
-sudo unzip ow-1.9.zip -d /opt
+unzip ow-1.9.zip -d /opt
 wget https://nav.dl.sourceforge.net/project/regina-rexx/regina-rexx/3.9.4/regina-rexx-3.9.4.tar.gz
-sudo tar xvzf regina-rexx-3.9.4.tar.gz -C ../src
-sudo chown -R ${ME}.${ME} /root/osfree /root/dist /root/src
+tar xvzf regina-rexx-3.9.4.tar.gz -C ../src
+chown -R ${ME}.${ME} /root/osfree /root/dist /root/src
 cd ../src/regina-rexx-3.9.4
 ./configure --prefix=/usr/local
-make && sudo make install
-sudo rm -rf ../regina-rexx-3.9.4
-sudo echo /usr/local/lib >>/etc/ld.so.conf.d/local.conf
-sudo ldconfig
+make && make install
+rm -rf ../regina-rexx-3.9.4
+echo /usr/local/lib >>/etc/ld.so.conf.d/local.conf
+ldconfig
 cd /root/osfree
