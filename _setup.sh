845d9be822b40f03dc1ac56cadc8a835a6d3e0e6
Valery Sedletski <_valerius@mail.ru>
2022-04-15 11:05:01 +0300

    Fix linux build #5

--- _setup.sh
+++ _setup.sh
@@ -5,22 +5,25 @@
 # Install osfree build deps
 #
 
-mkdir -p /root/osfree /root/dist /root/src
+ME=`whoami`
+CWD=`pwd`
+mkdir -p $CWD/dist $CWD/src $CWD/bin $CWD/osfree
+PATH=$CWD/bin:$PATH
 #echo "deb http://deb.debian.org/debian bullseye main contrib non-free" >/etc/apt/sources.list
 #echo "deb http://deb.debian.org/debian bullseye-updates main contrib non-free" >>/etc/apt/sources.list
 #echo "deb http://security.debian.org/debian-security bullseye-security main contrib non-free" >>/etc/apt/sources.list
 apt-get update
 apt-get install -y wget curl unzip tar gzip bash build-essential fpc
-cd /root/dist
+cd $CWD/dist
 wget ftp://osfree.org/upload/ow/ow-1.9.zip
 unzip ow-1.9.zip -d /opt
 wget https://nav.dl.sourceforge.net/project/regina-rexx/regina-rexx/3.9.4/regina-rexx-3.9.4.tar.gz
-tar xvzf regina-rexx-3.9.4.tar.gz -C ../src
-chown -R ${ME}.${ME} /root/osfree /root/dist /root/src
-cd ../src/regina-rexx-3.9.4
+tar xvzf regina-rexx-3.9.4.tar.gz -C $CWD/src
+chown -R ${ME}.${ME} $CWD/osfree $CWD/dist $CWD/src $CWD/bin
+cd $CWD/src/regina-rexx-3.9.4
 ./configure --prefix=/usr/local
-make && make install
-rm -rf ../regina-rexx-3.9.4
+CC=gcc CXX=g++ make && make install
+rm -rf $CWD/src/regina-rexx-3.9.4
 echo /usr/local/lib >>/etc/ld.so.conf.d/local.conf
 ldconfig
-cd /root/osfree
+cd $CWD/osfree
