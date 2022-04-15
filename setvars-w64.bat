aa9b9c725144f14a163d11a488820125cb3a6415
Valery Sedletski <_valerius@mail.ru>
2022-04-16 01:02:27 +0300

    Fix linux build #44

--- setvars-w64.bat
+++ setvars-w64.bat
@@ -10,10 +10,11 @@ set mkisofs=\data\dev\cdrtools\mkisofs.exe
 set serverenv=win32
 set env=WIN64
 set os=WIN64
-set REXX=c:\rexx
+set REGINA=c:\rexx
+set %REGINA%\regina
 set SERVERENV=win32
 
-set PATH=%WATCOM%\binnt64;%WATCOM%\binw;%TOOLS%;%FPPATH%;%svn%;%rexx%;\data\dev\qemu;\data\dev\cdrtools;\data\dev\Bochs-2.3.5;\data\dev\bin;\usr\local\wbin;%PATH%;
+set PATH=%WATCOM%\binnt64;%WATCOM%\binw;%TOOLS%;%FPPATH%;%svn%;%regina%;\data\dev\qemu;\data\dev\cdrtools;\data\dev\Bochs-2.3.5;\data\dev\bin;\usr\local\wbin;%PATH%;
 set INCLUDE=%WATCOM%\h;%WATCOM%\h\os21x;%WATCOM%\h\dos
 set LIB=%WATCOM%\lib386\nt
 set WD_PATH=%WATCOM%\binnt64
