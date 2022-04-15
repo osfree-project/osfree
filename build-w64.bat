f5bb9ff0a4c65a2420511ac6022b179a78a3d5f9
Valery Sedletski <_valerius@mail.ru>
2022-04-16 01:23:16 +0300

    Fix linux build #46

--- build-w64.bat
+++ build-w64.bat
@@ -1,4 +1,4 @@
-:@echo off
+@echo off
 setlocal
 rem Edit here to use your own *.conf
 rem set comspec=d:\os2\cmd.exe
@@ -7,5 +7,6 @@ rem set conf=git.conf
 rem --------------------------------
 rem c:\rexx\regina setenv.cmd %conf% >nul 2>&1
 call setvars-w64
+echo ROOT=%ROOT%
 wmake -h %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
