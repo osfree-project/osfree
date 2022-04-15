a00b00cbedc064d3b306343d431d01d072947399
Valery Sedletski <_valerius@mail.ru>
2022-04-15 14:21:49 +0300

    Fix linux build #15

--- build-lnx.sh
+++ build-lnx.sh
@@ -6,7 +6,7 @@
 #`which rexx` setenv.cmd ${conf} >/dev/null 2>&1
 . ./setvars-lnx.sh
 for var in $VARS; do
-    echo "$var=\"$($var)\""
+    echo "$var=\"$$var\""
 done
 echo "VARS=\"$VARS\""
 wmake -h $1 $2 $3 $4 $5 $6 $7 $8 $9
