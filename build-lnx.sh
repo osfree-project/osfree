b44242ad377501054174a79630715274716db602
Valery Sedletski <_valerius@mail.ru>
2022-04-15 15:10:55 +0300

    Fix linux build #18

--- build-lnx.sh
+++ build-lnx.sh
@@ -5,9 +5,8 @@
 # conf=docker.conf
 #`which rexx` setenv.cmd ${conf} >/dev/null 2>&1
 . ./setvars-lnx.sh
-for var in $VARS; do
-    vv=${$var}
-    echo "$var=\"$vv\""
-done
-echo "VARS=\"$VARS\""
+echo "VARS=$VARS"
+echo "OS=$OS"
+echo "SHELL=$SHELL"
+echo "REXX=$REXX"
 wmake -h $1 $2 $3 $4 $5 $6 $7 $8 $9
