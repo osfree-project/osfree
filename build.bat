703831410ccbc05b34140a6104dd3a3ddb832555
Valery Sedletski <_valerius@mail.ru>
2022-04-15 06:54:10 +0300

    Add some forgotten files

--- build.bat
+++ build.bat
@@ -5,6 +5,6 @@ rem set comspec=d:\os2\cmd.exe
 rem set os2_shell=d:\os2\cmd.exe
 set conf=note.conf
 rem --------------------------------
-call setenv.cmd %conf% >nul 2>&1
+c:\rexx\regina setenv.cmd %conf% >nul 2>&1
 wmake -h %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 #
 
 # Edit here to use your own *.conf
-conf=docker.conf
-`which rexx` setenv.cmd ${conf} >/dev/null 2>&1
+# conf=docker.conf
+#`which rexx` setenv.cmd ${conf} >/dev/null 2>&1
+. ./setvars.sh
 wmake -h $1 $2 $3 $4 $5 $6 $7 $8 $9
