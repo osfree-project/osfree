abca852ac8e625c35ed7ba2266ac72790f1ad891
Valery Sedletski <_valerius@mail.ru>
2022-04-17 02:39:57 +0300

    _setup.bat: Save files to c:/Documents and Settings/Downloads under windows

--- _setup.bat
+++ _setup.bat
@@ -3,24 +3,24 @@ rem Install wget
 choco install wget --no-progress
 
 rem Download latest Open Watcom
-wget https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/open-watcom-2_0-c-win-x64.exe
+wget https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/open-watcom-2_0-c-win-x64.exe -P "c:/Documents and settings/Downloads"
 dir
 
 rem Install OpenWatcom
-open-watcom-2_0-c-win-x64.exe -s -p -ns
+"c:/Documents and settings/Downloads/open-watcom-2_0-c-win-x64.exe" -s -p -ns
 
 rem Download Regina REXX
-wget https://sourceforge.net/projects/regina-rexx/files/regina-rexx/3.9.4/Regina394w64.exe
+wget https://sourceforge.net/projects/regina-rexx/files/regina-rexx/3.9.4/Regina394w64.exe -P "c:/Documents and settings/Downloads"
 
 rem Install Regina REXX
-Regina394w64.exe /S /D=C:\REXX
+"c:/Documents and settings/Downloads/Regina394w64.exe" /S /D=C:\REXX
 
 rem Download FPC
-wget  https://downloads.sourceforge.net/project/freepascal/Win32/3.2.2/fpc-3.2.2.i386-win32.exe
+wget  https://downloads.sourceforge.net/project/freepascal/Win32/3.2.2/fpc-3.2.2.i386-win32.exe -P "c:/Documents and settings/Downloads"
 dir
 
 rem install fpc
-fpc-3.2.2.i386-win32.exe /silent
+"c:/Documents and settings/Downloads/fpc-3.2.2.i386-win32.exe" /silent
 
 rem build
 set
