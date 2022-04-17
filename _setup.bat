5ebdca7ecf3f7deea7ba95c078cff908ba92e0d0
Valery Sedletski <_valerius@mail.ru>
2022-04-17 08:11:04 +0300

    _setup.bat: Fix a typo.

--- _setup.bat
+++ _setup.bat
@@ -4,12 +4,12 @@ choco install wget unzip --no-progress
 
 rem Download latest Open Watcom
 rem wget https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/open-watcom-2_0-c-win-x64.exe -P "c:/Documents and settings/Downloads"
-wget ftp://ftp.osfree.org/upload/ow/ow-19.zip -P "c:/Documents and settings/Downloads"
+wget ftp://ftp.osfree.org/upload/ow/ow-1.9.zip -P "c:/Documents and settings/Downloads"
 dir
 
 rem Install OpenWatcom
 rem "c:/Documents and settings/Downloads/open-watcom-2_0-c-win-x64.exe" -s -p -ns
-unzip "c:/Documents and settings/Downloads/ow-19.zip" -d c:\
+unzip "c:/Documents and settings/Downloads/ow-1.9.zip" -d c:\
 
 rem Download Regina REXX
 wget https://sourceforge.net/projects/regina-rexx/files/regina-rexx/3.9.4/Regina394w64.exe -P "c:/Documents and settings/Downloads"
