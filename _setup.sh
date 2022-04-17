20691f1173448ed70c4a1dd02542390e37d35e31
Valery Sedletski <_valerius@mail.ru>
2022-04-17 05:00:41 +0300

    _setup.sh: Use latest fpc distro from freepascal.org

--- _setup.sh
+++ _setup.sh
@@ -13,10 +13,14 @@ PATH=$_CWD/bin:$PATH
 #echo "deb http://deb.debian.org/debian bullseye-updates main contrib non-free" >>/etc/apt/sources.list
 #echo "deb http://security.debian.org/debian-security bullseye-security main contrib non-free" >>/etc/apt/sources.list
 apt-get update
-apt-get install -y wget curl unzip tar gzip bash build-essential fpc
+apt-get install -y wget curl unzip tar gzip bash
 cd $_CWD/dist
 wget ftp://osfree.org/upload/ow/ow-1.9.zip
 unzip ow-1.9.zip -d /opt
+wget ftp://ftp.hu.freepascal.org/pub/fpc/dist/3.2.2/x86_64-linux/fpc-3.2.2.x86_64-linux.tar
+tar xvf fpc-3.2.2.x86_64-linux.tar
+cd fpc-3.2.2.x86_64-linux
+echo "/opt/fpc-3.2.2" | ./install.sh
 wget https://nav.dl.sourceforge.net/project/regina-rexx/regina-rexx/3.9.4/regina-rexx-3.9.4.tar.gz
 tar xvzf regina-rexx-3.9.4.tar.gz -C $_CWD/src
 chown -R $_ME.$_ME $_CWD/osfree $_CWD/dist $_CWD/src $_CWD/bin
