a91fa2450b06a5d96e491c429f7f6b3c3fed8fe5
Valery Sedletski <_valerius@mail.ru>
2022-04-22 14:48:56 +0300

    Additional build fixes #11.

--- _wcc.sh
+++ _wcc.sh
@@ -3,6 +3,11 @@
 
 PATH=.:$PATH
 build-lnx.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
+echo -------------------------------------
+ls -la /home/runner/work/osfree/osfree/build/tools/critstrs
+echo -------------------------------------
+ls -la /home/runner/work/osfree/osfree/bin/tools
+echo -------------------------------------
 ls -la /home/runner/work/osfree/osfree/build/DOS/CMD/command/strings/critstrs
 echo -------------------------------------
 ls -la /home/runner/work/osfree/osfree/build/DOS/CMD/command
