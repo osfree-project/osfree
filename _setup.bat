643497057e83319736fd6d47f2b8f59239566dac
valerius2k <_valerius@mail.ru>
2022-04-30 00:54:19 +0000

    Misc. Win32 build fixes.

--- _setup.bat
+++ _setup.bat
@@ -1,4 +1,6 @@
 @echo off
+rem set path=e:\win32\bin;%path%
+
 rem Install wget
 choco install wget unzip --no-progress
 
@@ -11,20 +13,21 @@ rem "c:/Documents and settings/Downloads/open-watcom-2_0-c-win-x64.exe" -s -p -n
 unzip "c:/Documents and Settings/Downloads/ow-1.9.zip" -d c:\
 
 rem Download Regina REXX
-wget https://sourceforge.net/projects/regina-rexx/files/regina-rexx/3.9.4/Regina394w32.exe -P "c:/Documents and settings/Downloads"
+wget --no-check-certificate https://sourceforge.net/projects/regina-rexx/files/regina-rexx/3.9.4/Regina394w32.exe -P "c:/Documents and settings/Downloads"
 
 rem Install Regina REXX
 "c:\Documents and Settings\Downloads\Regina394w32.exe" /S /D=C:\REXX
 
 rem Download FPC
-wget  https://downloads.sourceforge.net/project/freepascal/Win32/3.2.2/fpc-3.2.2.i386-win32.exe -P "c:/Documents and settings/Downloads"
-wget  https://downloads.sourceforge.net/project/freepascal/OS_2/3.2.2/os2322.zip -P "c:/Documents and settings/Downloads"
+wget --no-check-certificate https://downloads.sourceforge.net/project/freepascal/Win32/3.2.2/fpc-3.2.2.i386-win32.exe -P "c:/Documents and settings/Downloads"
+wget --no-check-certificate https://downloads.sourceforge.net/project/freepascal/OS_2/3.2.2/os2322.zip -P "c:/Documents and settings/Downloads"
 
 rem install fpc
 "c:\Documents and Settings\Downloads\fpc-3.2.2.i386-win32.exe" /silent
 unzip "c:\Documents and Settings\Downloads\os2322.zip" -d "c:/Documents and settings/Downloads"
 unzip "c:\Documents and Settings\Downloads\baseos2.zip" units\* -dc:\fpc\3.2.2
-dir "c:/Documents and settings/Downloads"
-dir c:\fpc /s
+
+rem dir "c:/Documents and settings/Downloads"
+rem dir c:\fpc /s
 rem build
 rem call build-git.cmd
--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 @set os2_shell >nul 2>&1
-if errorlevel 2 goto _win
+if errorlevel 1 goto _win
 
 :_os2
 call build-os2 %1 %2 %3 %4 %5 %6 %7 %8 %9
