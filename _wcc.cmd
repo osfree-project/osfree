de481f3818192fdcfaa2d3a23f369a41fa7fc943
Valery Sedletski <_valerius@mail.ru>
2022-04-30 06:47:01 +0300

    Win64 build test.

--- _wcc.cmd
+++ _wcc.cmd
@@ -1,4 +1,1 @@
 call build %1 %2 %3 %4 %5 %6 %7 %8 %9
-echo ----------
-dir \a\osfree\osfree\build\tools\winrc
-echo ----------
--- build-w64.bat
+++ build-w64.bat
@@ -8,4 +8,7 @@ rem --------------------------------
 rem c:\rexx\regina setenv.cmd %conf% >nul 2>&1
 call setvars-w64
 wmake -h %1 %2 %3 %4 %5 %6 %7 %8 %9
+echo ----------
+dir \a\osfree\osfree\build\tools\winrc
+echo ----------
 endlocal
