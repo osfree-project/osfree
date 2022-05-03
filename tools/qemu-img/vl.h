b70c52b38fd77cbdd6ca3df0dc28c4f8373f934b
prokushev <yuri.prokushev@gmail.com>
2022-05-03 22:14:35 +0700

    missed file, ?32BIT symbol defined, warning fix

--- tools/qemu-img/vl.h
+++ tools/qemu-img/vl.h
@@ -52,11 +52,11 @@
 /* XXX: find 64 bit version */
 #define ftruncate chsize
 
-char *realpath(const char *path, char *resolved_path)
+/* char *realpath(const char *path, char *resolved_path)
 {
     _fullpath(resolved_path, path, _MAX_PATH);
     return resolved_path;
-}
+} */
 #endif
 
 #ifdef QEMU_TOOL
