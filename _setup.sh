a7deb97b65ac31df5ab77662f06082f8c1a31320
Valery Sedletski <_valerius@mail.ru>
2022-05-09 04:04:21 +0300

    Minor _setup.sh fix.

--- _setup.sh
+++ _setup.sh
@@ -19,13 +19,13 @@ wget ftp://osfree.org/upload/ow/ow-1.9.zip
 unzip -o ow-1.9.zip -d /opt
 wget https://downloads.sourceforge.net/project/freepascal/OS_2/3.2.2/os2322.zip
 unzip -o os2322.zip -d $_CWD/dist/os2
-for file in os2/*.zip; do
-  unzip -o $file units/* fpmkinst/* -d /usr/lib/fpc/3.2.2/
-done
 wget https://downloads.sourceforge.net/project/freepascal/Linux/3.2.2/fpc-3.2.2.i386-linux.tar
 tar xf fpc-3.2.2.i386-linux.tar
 cd fpc-3.2.2.i386-linux
 ./install.sh </dev/null
+for file in os2/*.zip; do
+  unzip -o $file units/* fpmkinst/* -d /usr/lib/fpc/3.2.2/
+done
 wget https://nav.dl.sourceforge.net/project/regina-rexx/regina-rexx/3.9.4/regina-rexx-3.9.4.tar.gz
 tar xvzf regina-rexx-3.9.4.tar.gz -C $_CWD/src
 chown -R $_ME.$_ME $_CWD/osfree $_CWD/dist $_CWD/src $_CWD/bin
