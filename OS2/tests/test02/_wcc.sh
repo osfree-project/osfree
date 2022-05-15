aa2f8dbe3dc06b3351cda72299d7b26c52e0b576
Valery Sedletski <_valerius@mail.ru>
2022-05-15 18:59:20 +0300

    Merge.

--- OS2/tests/test02/_wcc.sh
+++ OS2/tests/test02/_wcc.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 #
 
+<<<<<<< Updated upstream
 export ROOT=../../..
 export PATH=$ROOT/conf/scripts:$PATH
 build-lnx.sh $*
+=======
+PATH=../../..:$PATH
+build-lnx.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
+>>>>>>> Stashed changes
