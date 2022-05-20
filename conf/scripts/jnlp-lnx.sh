91b41e371313a3c1074f2586c0e0a8f49feb9866
Valery Sedletski <_valerius@mail.ru>
2022-05-21 01:18:30 +0300

    Add forgotten jnlp-lnx.sh parameter.

--- conf/scripts/jnlp-lnx.sh
+++ conf/scripts/jnlp-lnx.sh
@@ -3,7 +3,8 @@
 
 WORKSPACE=/root
 JENKINS_URL=http://10.8.10.18:8080/
-JNLP_SECRET=
+NODE_NAME=linux-docker-i386
+JNLP_SECRET=c215e3e5695b58835883bc6bc6040f8479a7ae5c8d56409f30d2452cd728213c
 
 echo "Downloading the agent"
 wget ${JENKINS_URL}jnlpJars/agent.jar -O /root/agent.jar
