9214aea960d753343f8f81544f304408ad9e498f
prokushev <yuri.prokushev@gmail.com>
2022-07-01 21:41:16 +0700

    missed file added

---
+++ OS2/PM/API/PMGPI/gpi16primitives.c
@@ -0,0 +1,778 @@
+#ifndef GPI16PRIMITIVES_INCLUDED
+#define GPI16PRIMITIVES_INCLUDED
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIBEGINAREA(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIBOX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPICHARSTRING(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPICHARSTRINGAT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIENDAREA(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPILINE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIMOVE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIPOLYLINE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+//COLORREF WINAPI GetTextColor(HDC);
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYCOLOR(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYPATTERN(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+//COLORREF WINAPI SetTextColor(HDC, COLORREF);
+// fix prototype !!!
+USHORT APIENTRY16 GPISETCOLOR(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETPATTERN(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPICHARSTRINGPOS(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPICHARSTRINGPOSAT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPICOMMENT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIFULLARC(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIIMAGE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIMARKER(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIPARTIALARC(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIPOINTARC(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIPOLYFILLET(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIPOLYFILLETSHARP(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIPOLYMARKER(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIPOLYSPLINE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIPOP(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIPTVISIBLE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYARCPARAMS(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYATTRMODE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYATTRS(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+//COLORREF WINAPI GetBkColor(HDC);
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYBACKCOLOR(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYBACKMIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYCHARANGLE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYCHARBOX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYCHARDIRECTION(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYCHARMODE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYCHARSET(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYCHARSHEAR(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYCHARSTRINGPOS(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYCHARSTRINGPOSAT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYCURRENTPOSITION(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYDEFCHARBOX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYLINEEND(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYLINEJOIN(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYLINETYPE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYLINEWIDTH(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYLINEWIDTHGEOM(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYMARKER(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYMARKERBOX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYMARKERSET(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYMIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYPATTERNREFPOINT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYPATTERNSET(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYTEXTBOX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIRECTVISIBLE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETARCPARAMS(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETATTRMODE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETATTRS(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+//COLORREF WINAPI SetBkColor(HDC, COLORREF);
+// fix prototype !!!
+USHORT APIENTRY16 GPISETBACKCOLOR(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETBACKMIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETCHARANGLE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETCHARBOX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETCHARDIRECTION(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETCHARMODE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETCHARSET(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETCHARSHEAR(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETCURRENTPOSITION(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETLINEEND(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETLINEJOIN(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETLINETYPE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETLINEWIDTH(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETLINEWIDTHGEOM(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETMARKER(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETMARKERBOX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETMARKERSET(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETMIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETPATTERNREFPOINT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETPATTERNSET(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+#endif
