488fb885fa4f8a7e91cf7cdc196ff7274b7dcad7
prokushev <yuri.prokushev@gmail.com>
2022-07-01 15:10:17 +0700

    PMGPI improvements

---
+++ OS2/PM/API/PMGPI/gpi16segediting.c
@@ -0,0 +1,154 @@
+#ifndef GPI16SEGEDITING_INCLUDED
+#define GPI16SEGEDITING_INCLUDED
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETEDITMODE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYEDITMODE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETELEMENTPOINTER(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYELEMENTPOINTER(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIOFFSETELEMENTPOINTER(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIDELETEELEMENT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIDELETEELEMENTRANGE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPILABEL(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETELEMENTPOINTERATLABEL(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIDELETEELEMENTSBETWEENLABELS(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYELEMENTTYPE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYELEMENT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIELEMENT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIBEGINELEMENT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIENDELEMENT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+#endif
---
+++ OS2/PM/API/PMGPI/gpi16transforms.c
@@ -0,0 +1,194 @@
+#ifndef GPI16TRANSFORMS_INCLUDED
+#define GPI16TRANSFORMS_INCLUDED
+
+// fix prototype !!!
+USHORT APIENTRY16 GPICONVERT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPICALLSEGMENTMATRIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYDEFAULTVIEWMATRIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYGRAPHICSFIELD(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYMODELTRANSFORMMATRIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYPAGEVIEWPORT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYSEGMENTTRANSFORMMATRIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYVIEWINGTRANSFORMMATRIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIQUERYVIEWINGLIMITS(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPIROTATE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISCALE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETDEFAULTVIEWMATRIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETGRAPHICSFIELD(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETMODELTRANSFORMMATRIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETPAGEVIEWPORT(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETSEGMENTTRANSFORMMATRIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETVIEWINGLIMITS(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPISETVIEWINGTRANSFORMMATRIX(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+// fix prototype !!!
+USHORT APIENTRY16 GPITRANSLATE(void)
+{
+#ifdef BIT32
+	return unimplemented(__FUNCTION__);
+#else
+	return unimplemented(__FUNCTION__);
+#endif
+}
+
+#endif
