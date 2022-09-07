d8259458a135f3d01845c2f7b3734bfc2e71e4f2
prokushev <yuri.prokushev@gmail.com>
2022-09-07 09:07:58 +0700

    pmgpi -> pmgre stub test

--- OS2/PM/API/PMGPI/gpi32dev.c
+++ OS2/PM/API/PMGPI/gpi32dev.c
@@ -1,14 +1,24 @@
 #ifndef GPI32DEV_INCLUDED
 #define GPI32DEV_INCLUDED
 
+#define INCL_GREALL
+#include <pmddi.h>
+
+/*
+  Documentation: http://www.edm2.com/index.php/GreCloseDC
+*/
 HMF    APIENTRY DevCloseDC(HDC hdc)
 {
-  return unimplemented(__FUNCTION__);
+   return GreCloseDC(hdc);
 }
+
+/*
+  Documentation: http://www.edm2.com/index.php/GreOpenDC
+*/
 
 HDC    APIENTRY DevOpenDC(HAB hab,LONG a,PCSZ b,LONG c,PDEVOPENDATA d,HDC hdc)
 {
-	#include "devopendc.c"
+   return GreOpenDC(hdc, a, b, c, d);
 }
 
 LONG   APIENTRY DevPostDeviceModes(HAB hab,PDRIVDATA a,PCSZ b,PCSZ c,PCSZ d,ULONG e)
@@ -16,14 +26,21 @@ LONG   APIENTRY DevPostDeviceModes(HAB hab,PDRIVDATA a,PCSZ b,PCSZ c,PCSZ d,ULON
   return unimplemented(__FUNCTION__);
 }
 
+/*
+  Documentation: http://www.edm2.com/index.php/GreQueryDeviceCaps
+*/
+
 BOOL   APIENTRY DevQueryCaps(HDC hdc,LONG a,LONG b,PLONG c)
 {
-  return unimplemented(__FUNCTION__);
+  return GreQueryDeviceCaps(hdc, a, b, c);
 }
 
+/*
+  Documentation: http://www.edm2.com/index.php/GreEscape
+*/
 LONG   APIENTRY DevEscape(HDC hdc,LONG a,LONG b,PBYTE c,PLONG d,PBYTE e)
 {
-  return unimplemented(__FUNCTION__);
+  return GreEscape(hdc, a, b, c, d, e);
 }
 
 LONG   APIENTRY DevPostEscape(PCSZ a,PCSZ b,PCSZ c,PCSZ d,ULONG e,ULONG f,PBYTE g,ULONG h,PBYTE i)
@@ -36,9 +53,12 @@ BOOL   APIENTRY DevQueryDeviceNames(HAB hab,PCSZ a,PLONG b,PSTR32 c,PSTR64 d,PLO
   return unimplemented(__FUNCTION__);
 }
 
+/*
+  Documentation: http://www.edm2.com/index.php/GreQueryHardcopyCaps
+*/
 LONG   APIENTRY DevQueryHardcopyCaps(HDC hdc,LONG a,LONG b,PHCINFO c)
 {
-  return unimplemented(__FUNCTION__);
+  return GreQueryHardcopyCaps(hdc,a,b,c);
 }
 
 #endif
