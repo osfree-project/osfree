146ab6807f11030f69f7eb2388e0f02356205175
Valery Sedletski <_valerius@mail.ru>
2023-01-25 23:39:39 +0300

    Support for patching port sources.

--- tools/mk/port.mk
+++ tools/mk/port.mk
@@ -15,17 +15,23 @@ prep: .symbolic
  @if not exist $(CONTRIB)$(PORT_NAME).flg $(MAKE) $(MAKEOPT) prep_svn
 !endif
 
+patch: .symbolic
+ $(verbose)$(SAY) PATCH    $(PORT_NAME) $(LOG)
+ $(verbose)for %i in ($(PORT_PATCHES)) do $(CD) $(PORT_BASE) && patch -p1 <$(MYDIR)patches$(SEP)%i
+
 prep_wget: .symbolic
  $(verbose)$(SAY) PREP     $(PORT_NAME) $(LOG)
  $(verbose)wget $(PORT_URL) -O $(%TMP)$(SEP)qw.zip
  $(verbose)unzip -o $(%TMP)$(SEP)qw.zip -d $(PORT_BASE)
  $(verbose)$(DC) $(%TMP)$(SEP)qw.zip
+ $(verbose)if exist $(MYDIR)patches $(MAKE) $(MAKEOPT) patch
  $(verbose)wtouch $(CONTRIB)$(PORT_NAME).flg
 
 prep_git: .symbolic
  $(verbose)$(SAY) PREP     $(PORT_NAME) $(LOG)
  $(verbose)git clone $(PORT_URL) $(PORT_BASE)
  $(verbose)$(CD) $(PORT_BASE) && git checkout $(PORT_REV)
+ $(verbose)if exist $(MYDIR)patches $(MAKE) $(MAKEOPT) patch
  $(verbose)wtouch $(CONTRIB)$(PORT_NAME).flg
 
 prep_svn: .symbolic
