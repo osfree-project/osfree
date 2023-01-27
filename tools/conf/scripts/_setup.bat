2d2a0f0c0d7d6ff3f5bba119b13e1c76deb6f5c5
prokushev <yuri.prokushev@gmail.com>
2023-01-27 13:46:18 +0700

    wget-win32 added

--- tools/conf/scripts/_setup.bat
+++ tools/conf/scripts/_setup.bat
@@ -5,14 +5,15 @@ rem https://eternallybored.org/misc/wget/1.19.4/32/wget.exe
 rem set path=e:\win32\bin;%path%
 rem or...
 rem Install wget
-choco install wget --no-progress
+rem choco install wget --no-progress
+set wget=..\wget-win32.exe
 
-wget http://www2.cs.uidaho.edu/~jeffery/win32/unzip.exe -P "%tmp%" -c
+%wget% http://www2.cs.uidaho.edu/~jeffery/win32/unzip.exe -P "%tmp%" -c
 
 rem Download latest Open Watcom
 rem wget https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/open-watcom-2_0-c-win-x64.exe -P "c:/Documents and settings/Downloads"
 rem wget ftp://ftp.osfree.org/upload/ow/ow-1.9.zip -P "c:/Documents and Settings/Downloads"
-wget ftp://ftp.osfree.org/upload/ow/ow-1.9.zip -P "%tmp%" -c
+%wget% ftp://ftp.osfree.org/upload/ow/ow-1.9.zip -P "%tmp%" -c
 
 rem Install OpenWatcom
 rem "c:/Documents and settings/Downloads/open-watcom-2_0-c-win-x64.exe" -s -p -ns
@@ -27,16 +28,17 @@ rem "%tmp%\unzip" -o "%tmp%/JWasm_v215_win32.zip" JWasm.exe -d c:\watcom\binnt
 
 rem Download Regina REXX
 rem wget --no-check-certificate https://sourceforge.net/projects/regina-rexx/files/regina-rexx/3.9.4/Regina394w32.exe -P "c:/Documents and settings/Downloads"
-wget --no-check-certificate https://sourceforge.net/projects/regina-rexx/files/regina-rexx/3.9.4/Regina394w32.exe -P "%tmp%" -c
+%wget% --no-check-certificate https://sourceforge.net/projects/regina-rexx/files/regina-rexx/3.9.4/Regina394w32.exe -P "%tmp%" -c
 
 rem Install Regina REXX
+if exist C:\REXX\uninstall.exe C:\REXX\uninstall /S
 "%tmp%\Regina394w32.exe" /S /D=C:\REXX
 
 rem Download FPC
 rem wget --no-check-certificate https://downloads.sourceforge.net/project/freepascal/Win32/3.2.2/fpc-3.2.2.i386-win32.exe -P "c:/Documents and settings/Downloads"
 rem wget --no-check-certificate https://downloads.sourceforge.net/project/freepascal/OS_2/3.2.2/os2322.zip -P "c:/Documents and settings/Downloads"
-wget --no-check-certificate https://downloads.sourceforge.net/project/freepascal/Win32/3.2.2/fpc-3.2.2.i386-win32.exe -P "%tmp%" -c
-wget --no-check-certificate https://downloads.sourceforge.net/project/freepascal/OS_2/3.2.2/os2322.zip -P "%tmp%" -c
+%wget% --no-check-certificate https://downloads.sourceforge.net/project/freepascal/Win32/3.2.2/fpc-3.2.2.i386-win32.exe -P "%tmp%" -c
+%wget% --no-check-certificate https://downloads.sourceforge.net/project/freepascal/OS_2/3.2.2/os2322.zip -P "%tmp%" -c
 
 rem install fpc
 "%tmp%\fpc-3.2.2.i386-win32.exe" /silent
