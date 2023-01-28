a8f0e741faa21656a053f51a4868db84a2a92e22
prokushev <yuri.prokushev@gmail.com>
2023-01-28 23:36:14 +0700

    IB now port

---
+++ OS2/MM/ib/ib.mk
@@ -0,0 +1,8 @@
+# $Id: makefile,v 1.1 2004/08/16 06:27:05 prokushev Exp $
+
+PORT_NAME = ib
+PORT_TYPE = git
+PORT_URL  = https://github.com/OS2World/APP-GRAPHICS-FM2_Image
+PORT_REV  = 
+
+!include $(%ROOT)tools/mk/port.mk
