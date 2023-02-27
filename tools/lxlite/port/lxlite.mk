e123d6baed2a3ef2c2b586f6408b660b4a9fc9b9
Valery Sedletski <_valerius@mail.ru>
2023-02-28 00:59:56 +0300

    Finish lxlite port.

---
+++ tools/lxlite/port/lxlite.mk
@@ -0,0 +1,9 @@
+# $Id: makefile,v 1.1 2004/08/16 06:27:05 prokushev Exp $
+
+PORT_NAME = lxlite
+PORT_TYPE = git
+PORT_URL  = https://github.com/bitwiseworks/lxlite
+PORT_REV  = master
+PORT_PATCHES  = lxlite.diff
+
+!include $(%ROOT)tools/mk/port.mk
