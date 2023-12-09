e775537ad85f4f77aea330053d8101d864aa6c74
prokushev <yuri.prokushev@gmail.com>
2023-12-09 21:57:21 +0700

    sync

---
+++ OS2/WPS/PMWP/shl.c
@@ -0,0 +1,15 @@
+APIRET APIENTRY ShlSaveEnv(PEXCEPTIONREGISTRATIONRECORD pERegRec, PVOID handler)
+{
+}
+
+APIRET APIENTRY ShlStartWorkplace(HAB hab, HMQ hmq)
+{
+}
+
+APIRET APIENTRY MessageLoopProc(HAB hab)
+{
+}
+
+APIRET APIENTRY ShlExceptionHandler(void)
+{
+}
