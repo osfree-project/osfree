fe7d03536b48038c57ced31b9ccdf034b8c33225
prokushev <yuri.prokushev@gmail.com>
2024-04-11 11:47:36 +0700

    vio subsystem routers...

--- OS2/CPI/sub32/vio.c
+++ OS2/CPI/sub32/vio.c
@@ -3,7 +3,7 @@
 #include <os2.h>
 
 APIRET unimplemented(char *func);
-//APIRET APIENTRY BVSMain(ULONG fn);
+//APIRET APIENTRY BVSMain(ULONG fn, ARGS args);
 
 // Function numbers for Base Video System main call
 #define FN_GETPHYSBUF      0
@@ -48,8 +48,224 @@ APIRET unimplemented(char *func);
 #define FN_GETSTATE        39
 #define FN_SETSTATE        40
 
+typedef
+union
+{
+struct 	{
+			PVIOPHYSBUF pvioPhysBuf; 
+		} GetPhysBuf;
+struct 	{
+			PULONG pLVB;
+			PUSHORT pcbLVB;
+			HVIO hvio; 
+		} GetBuf;
+struct	{
+			USHORT offLVB;
+			USHORT cb;
+			HVIO hvio;
+		} ShowBuf;
+struct	{
+			USHORT * Row;
+			USHORT * Column;
+			HVIO Handle;
+		} GetCurPos;
+struct	{
+			VIOCURSORINFO * CursorInfo;
+			HVIO Handle;
+		} GetCurType;
+struct	{
+			VIOMODEINFO * ModeInfo;
+			HVIO Handle;
+		} GetMode;
+struct	{
+			USHORT Row;
+			USHORT Column;
+			HVIO Handle;
+		} SetCurPos;
+struct	{
+			PVIOCURSORINFO CursorInfo;
+			HVIO Handle;
+		} SetCurType;
+struct	{
+			PVIOMODEINFO ModeInfo;
+			HVIO hvio;
+		} SetMode;
+struct	{
+			CHAR * CellStr;
+			USHORT * Count;
+			USHORT Row;
+			USHORT Column;
+			HVIO Handle;
+		} ReadCharStr;
+struct	{
+			CHAR * CellStr;
+			USHORT * Count;
+			USHORT Row;
+			USHORT Column;
+			HVIO Handle;
+		} ReadCellStr;
+struct	{
+			PCHAR Char;
+			USHORT Count;
+			USHORT Row;
+			USHORT Column;
+			HVIO Handle;
+		} WrtNChar;
+struct	{
+			PBYTE Attr;
+			USHORT Count;
+			USHORT Row;
+			USHORT Column;
+			HVIO Handle;
+		} WrtNAttr;
+struct	{
+			PBYTE Cell;
+			USHORT Count;
+			USHORT Row;
+			USHORT Column;
+			HVIO Handle;
+		} WrtNCell;
+struct	{
+			PCHAR Str;
+			USHORT Count;
+			USHORT Row;
+			USHORT Column;
+			HVIO Handle;
+		} WrtCharStr;
+struct	{ 
+			PCHAR Str;
+			USHORT Count;
+			USHORT Row;
+			USHORT Column;
+			PBYTE pAttr;
+			HVIO Handle;
+		} WrtCharStrAtt;
+struct	{
+			PCHAR CellStr;
+			USHORT Count;
+			USHORT Row;
+			USHORT Column;
+			HVIO Handle;
+		} WrtCellStr;
+struct	{ 
+			USHORT TopRow;
+			USHORT LeftCol;
+			USHORT BotRow;
+			USHORT RightCol;
+			USHORT Lines;
+			PBYTE Cell;
+			HVIO Handle;
+		} ScrollUp;
+struct	{
+			USHORT TopRow;
+			USHORT LeftCol;
+			USHORT BotRow;
+			USHORT RightCol;
+			USHORT Lines;
+			PBYTE Cell;
+			HVIO Handle;
+		} ScrollDn;
+struct	{
+			USHORT usTopRow;
+			USHORT usLeftCol;
+			USHORT usBotRow;
+			USHORT usRightCol;
+			USHORT cbCol;
+			PBYTE pCell;
+			HVIO hvio;
+		} ScrollLf;
+struct	{ 
+			USHORT usTopRow;
+			USHORT usLeftCol;
+			USHORT usBotRow;
+			USHORT usRightCol;
+			USHORT cbCol;
+			PBYTE pCell;
+			HVIO hvio;
+		} ScrollRt;
+struct	{ 
+			USHORT Ansi;
+			HVIO Handle;
+		} SetAnsi;
+struct	{ 
+			USHORT * Ansi;
+			HVIO Handle;
+		} GetAnsi;
+struct	{ 
+			HVIO hvio;
+		} PrtSc;
+struct	{
+			USHORT fWait;
+			PUCHAR pfNotLocked;
+			HVIO hvio;
+		} ScrLock;
+struct	{
+			HVIO hvio;
+		} ScrUnLock;
+struct	{
+			USHORT usRedrawInd;
+			PUSHORT pNotifyType;
+		} SavRedrawWait;
+struct	{
+			USHORT usOwnerInd;
+			USHORT usKillInd;
+		} SavRedrawUndo;
+struct	{ 
+			PUSHORT pfWait;
+			HVIO hvio;
+		} PopUp;
+struct	{ 
+			HVIO hvio;
+		} EndPopUp;
+struct	{ 
+			HVIO hvio;
+		} PrtScToggle;
+struct	{
+			USHORT usReqType;
+			PUSHORT pNotifyType;
+		} ModeWait;	
+struct	{ 
+			USHORT usOwnerInd;
+			USHORT usKillInd;
+		} ModeUndo;
+struct	{
+			PVIOFONTINFO pviofi;
+			HVIO hvio;
+		} GetFont;
+struct	{
+			USHORT ConfigId;
+			VIOCONFIGINFO * vioin;
+			HVIO hvio;
+		} GetConfig;
+struct	{ 
+			USHORT IdCodePage;
+			HVIO Handle;
+		} SetCp;
+struct	{ 
+			USHORT * IdCodePage;
+			HVIO Handle;
+		} GetCp;
+struct	{ 
+			PVIOFONTINFO pviofi;
+			HVIO hvio;
+		} SetFont;
+struct	{ 
+			PVOID pState;
+			HVIO Handle;
+		} GetState;
+struct	{ 
+			PVOID pState;
+			HVIO Handle;
+		} SetState;
+struct	{ 
+			PCHAR Str;
+			USHORT Count;
+			HVIO Handle;
+		} WrtTTY;
+} ARGS, * PARGS;
+
 // Call VIO function
-APIRET APIENTRY VioCall(ULONG fn)
+APIRET APIENTRY VioCall(ULONG fn, PARGS pargs)
 {
   return 0; //BVSMain(fn);
 }
@@ -74,22 +290,25 @@ APIRET APIENTRY VioInit(VOID)
    if (rc != NO_ERROR) {
       //printf("DosLoadModule error: return code = %u\n", rc);
       return 1;
-   } else {
-      //printf("Module %s loaded.\n", ModuleName);
-   } /* endif */
+   }
+   
    // Find 32-bit version
    rc = DosQueryProcAddr(ModuleHandle,         /* Handle to module           */
                          0,                    /* ProcName specified      */
                          "BVS32MAIN",            /* ProcName (not specified)   */
                          &ModuleAddr);         /* Address returned           */
+						 
    if (rc == ERROR_INVALID_NAME) 
    { // if no 32bit, try 16-bit
      rc = DosQueryProcAddr(ModuleHandle,         /* Handle to module           */
                            0,                    /* ProcName specified      */
                            "BVSMAIN",            /* ProcName (not specified)   */
                            &ModuleAddr);         /* Address returned           */
+	 
    };
-   rc = DosFreeModule(ModuleHandle);
+   
+   if (!rc) rc = DosFreeModule(ModuleHandle);
+   
    return NO_ERROR;
 }
 
@@ -181,145 +400,275 @@ USHORT APIENTRY VioGlobalReg(const PSZ pszModName, const PSZ pszEntryName, const
 
 USHORT APIENTRY VioGetAnsi(USHORT * Ansi, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.GetAnsi.Ansi=Ansi;
+	args.GetAnsi.Handle=Handle;
+	
+	return VioCall(FN_SETANSI, &args);
 }
 
 
 USHORT APIENTRY VioGetCp(const USHORT Reserved, USHORT * IdCodePage, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.GetCp.IdCodePage=IdCodePage;
+	args.GetCp.Handle=Handle;
+	
+	return VioCall(FN_GETCP, &args);
 }
 
 
 USHORT APIENTRY VioGetCurPos(USHORT * Row, USHORT * Column, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.GetCurPos.Row=Row;
+	args.GetCurPos.Column=Column;
+	args.GetCurPos.Handle=Handle;
+  
+	return VioCall(FN_GETCURPOS, &args);
 }
 
 
 USHORT APIENTRY VioGetCurType(VIOCURSORINFO * CursorInfo, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.GetCurType.CursorInfo=CursorInfo;
+	args.GetCurType.Handle=Handle;
+	
+	return VioCall(FN_GETCURTYPE, &args);
 }
 
 
 USHORT APIENTRY VioGetMode(VIOMODEINFO * ModeInfo, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.GetMode.ModeInfo=ModeInfo;
+	args.GetMode.Handle=Handle;
+	
+	return VioCall(FN_GETMODE, &args);
 }
 
 
 USHORT APIENTRY VioReadCellStr(CHAR * CellStr, USHORT * Count, const USHORT Row, const USHORT Column, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.ReadCellStr.CellStr=CellStr;
+	args.ReadCellStr.Count=Count;
+	args.ReadCellStr.Row=Row;
+	args.ReadCellStr.Column=Column;
+	args.ReadCellStr.Handle=Handle;
+	
+	return VioCall(FN_READCELLSTR, &args);
 }
 
 
 USHORT APIENTRY VioReadCharStr(CHAR * CellStr, USHORT * Count, const USHORT Row, const USHORT Column, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
-}
-
-
-USHORT APIENTRY VioScrollDown(const USHORT TopRow, const USHORT LeftCol, const USHORT BotRow, const USHORT RightCol, const USHORT Lines, const PBYTE Cell, const HVIO Handle)
-{
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.ReadCharStr.CellStr=CellStr;
+	args.ReadCharStr.Count=Count;
+	args.ReadCharStr.Row=Row;
+	args.ReadCharStr.Column=Column;
+	args.ReadCharStr.Handle=Handle;
+	
+	return VioCall(FN_READCHARSTR, &args);
 }
 
 
 USHORT APIENTRY VioScrollDn(const USHORT TopRow, const USHORT LeftCol, const USHORT BotRow, const USHORT RightCol, const USHORT Lines, const PBYTE Cell, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.ScrollDn.TopRow=TopRow;
+	args.ScrollDn.LeftCol=LeftCol;
+	args.ScrollDn.BotRow=BotRow;
+	args.ScrollDn.RightCol=RightCol;
+	args.ScrollDn.Lines=Lines;
+	args.ScrollDn.Cell=Cell;
+	args.ScrollDn.Handle=Handle;
+	
+	return VioCall(FN_SCROLLDN, &args);
 }
 
 
-USHORT APIENTRY VioScrollLeft(const USHORT TopRow, const USHORT LeftCol, const USHORT BotRow, const USHORT RightCol, const USHORT Columns, const PBYTE Cell, const HVIO Handle)
+USHORT APIENTRY VioScrollLf(const USHORT TopRow, const USHORT LeftCol, const USHORT BotRow, const USHORT RightCol, const USHORT Columns, const PBYTE Cell, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
-}
+	ARGS args;
 
 
-USHORT APIENTRY VioScrollRight(const USHORT TopRow, const USHORT LeftCol, const USHORT BotRow, const USHORT RightCol, const USHORT Columns, const PBYTE Cell, const HVIO Handle)
-{
-  return unimplemented(__FUNCTION__);
+	args.ScrollLf.usTopRow=TopRow;
+	args.ScrollLf.usLeftCol=LeftCol;
+	args.ScrollLf.usBotRow=BotRow;
+	args.ScrollLf.usRightCol=RightCol;
+	args.ScrollLf.cbCol=Columns;
+	args.ScrollLf.pCell=Cell;
+	args.ScrollLf.hvio=Handle;
+	
+	return VioCall(FN_SCROLLLF, &args);
 }
 
 
 USHORT APIENTRY VioScrollUp(const USHORT TopRow, const USHORT LeftCol, const USHORT BotRow, const USHORT RightCol, const USHORT Lines, const PBYTE Cell, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.ScrollUp.TopRow=TopRow;
+	args.ScrollUp.LeftCol=LeftCol;
+	args.ScrollUp.BotRow=BotRow;
+	args.ScrollUp.RightCol=RightCol;
+	args.ScrollUp.Lines=Lines;
+	args.ScrollUp.Cell=Cell;
+	args.ScrollUp.Handle=Handle;
+	
+	return VioCall(FN_SCROLLUP, &args);
 }
 
 
 USHORT APIENTRY VioSetAnsi(const USHORT Ansi, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.SetAnsi.Ansi=Ansi;
+	args.SetAnsi.Handle=Handle;
+	
+	return VioCall(FN_SETANSI, &args);
 }
 
 
 USHORT APIENTRY VioSetCp(const USHORT Reserved, const USHORT IdCodePage, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.SetCp.IdCodePage=IdCodePage;
+	args.SetCp.Handle=Handle;
+  
+	return VioCall(FN_SETCP, &args);
 }
 
 
 USHORT APIENTRY VioSetCurPos(const USHORT Row, const USHORT Column, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.SetCurPos.Row=Row;
+	args.SetCurPos.Column=Column;
+	args.SetCurPos.Handle=Handle;
+	
+	return VioCall(FN_SETCURPOS, &args);
 }
 
 
 USHORT APIENTRY VioSetCurType(const PVIOCURSORINFO CursorInfo, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.SetCurType.CursorInfo=CursorInfo;
+	args.SetCurType.Handle=Handle;
+	
+	return VioCall(FN_SETCURTYPE, &args);
 }
 
 
 USHORT APIENTRY VioSetMode(const PVIOMODEINFO ModeInfo, const HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.SetMode.ModeInfo=ModeInfo;
+	args.SetMode.hvio=hvio;
+	
+	return VioCall(FN_SETMODE, &args);
 }
 
 
 USHORT APIENTRY VioWrtCellStr(const PCHAR CellStr, const USHORT Count, const USHORT Row, const USHORT Column, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.WrtCellStr.CellStr=CellStr;
+	args.WrtCellStr.Count=Count;
+	args.WrtCellStr.Row=Row;
+	args.WrtCellStr.Column=Column;
+	args.WrtCellStr.Handle=Handle;
+	
+	return VioCall(FN_WRTCELLSTR, &args);
 }
 
 
 USHORT APIENTRY VioWrtCharStr(const PCHAR Str, const USHORT Count, const USHORT Row, const USHORT Column, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.WrtCharStr.Str=Str;
+	args.WrtCharStr.Count=Count;
+	args.WrtCharStr.Row=Row;
+	args.WrtCharStr.Column=Column;
+	args.WrtCharStr.Handle=Handle;
+	
+	return VioCall(FN_WRTCHARSTR, &args);
 }
 
 
-USHORT APIENTRY VioWrtCharStrAttr(const PCHAR Str, const USHORT Count, const USHORT Row, const USHORT Column, const PBYTE pAttr, const HVIO Handle)
+USHORT APIENTRY VioWrtCharStrAtt(const PCHAR Str, const USHORT Count, const USHORT Row, const USHORT Column, const PBYTE pAttr, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
-}
-
-
-USHORT APIENTRY VioWrtCharStrAtt(const PCCH pch, const USHORT cb, const USHORT usRow, const USHORT usColumn, const PBYTE pAttr, const HVIO hvio)
-{
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.WrtCharStrAtt.Str=Str;
+	args.WrtCharStrAtt.Count=Count;
+	args.WrtCharStrAtt.Row=Row;
+	args.WrtCharStrAtt.Column=Column;
+	args.WrtCharStrAtt.pAttr=pAttr;
+	args.WrtCharStrAtt.Handle=Handle;
+  
+	return VioCall(FN_WRTCHARSTRATT, &args);
 }
 
 
 USHORT APIENTRY VioWrtNAttr(const PBYTE Attr, const USHORT Count, const USHORT Row, const USHORT Column, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.WrtNAttr.Attr=Attr;
+	args.WrtNAttr.Count=Count;
+	args.WrtNAttr.Row=Row;
+	args.WrtNAttr.Column=Column;
+	args.WrtNAttr.Handle=Handle;
+	
+	return VioCall(FN_WRTNATTR, &args);
 }
 
 
 USHORT APIENTRY VioWrtNCell(const PBYTE Cell, const USHORT Count, const USHORT Row, const USHORT Column, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.WrtNCell.Cell=Cell;
+	args.WrtNCell.Count=Count;
+	args.WrtNCell.Row=Row;
+	args.WrtNCell.Column=Column;
+	args.WrtNCell.Handle=Handle;
+	
+	return VioCall(FN_WRTNCELL, &args);
 }
 
 
 USHORT APIENTRY VioWrtNChar(const PCHAR Char, const USHORT Count, const USHORT Row, const USHORT Column, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.WrtNChar.Char=Char;
+	args.WrtNChar.Count=Count;
+	args.WrtNChar.Row=Row;
+	args.WrtNChar.Column=Column;
+	args.WrtNChar.Handle=Handle;
+	
+	return VioCall(FN_WRTNCHAR, &args);
 }
 
 
@@ -328,39 +677,68 @@ USHORT APIENTRY VioWrtNChar(const PCHAR Char, const USHORT Count, const USHORT R
 
 USHORT APIENTRY VioGetState(const PVOID pState, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.GetState.pState=pState;
+	args.GetState.Handle=Handle;
+	
+	return VioCall(FN_GETSTATE, &args);
 }
 
 
 USHORT APIENTRY VioSetState(const PVOID pState, const HVIO Handle)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.SetState.pState=pState;
+	args.SetState.Handle=Handle;
+	
+	return VioCall(FN_SETSTATE, &args);
 }
 
 
 USHORT APIENTRY VioGetConfig(const USHORT ConfigId, VIOCONFIGINFO * vioin, const HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.GetConfig.ConfigId=ConfigId;
+	args.GetConfig.vioin=vioin;
+	args.GetConfig.hvio=hvio;
+	
+	return VioCall(FN_GETCONFIG, &args);
 }
 
 
 USHORT APIENTRY VioPopUp (PUSHORT pfWait,
                          HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.PopUp.pfWait=pfWait;
+	args.PopUp.hvio=hvio;
+	
+	return VioCall(FN_POPUP, &args);
 }
 
 
 USHORT APIENTRY VioGetPhysBuf (PVIOPHYSBUF pvioPhysBuf,
                               USHORT usReserved)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.GetPhysBuf.pvioPhysBuf=pvioPhysBuf;
+	
+	return VioCall(FN_GETPHYSBUF, &args);
 }
 
 
 USHORT APIENTRY VioPrtSc (HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.PrtSc.hvio=hvio;
+	
+	return VioCall(FN_PRTSC, &args);
 }
 
 
@@ -372,7 +750,17 @@ USHORT APIENTRY VioScrollRt (USHORT usTopRow,
                             PBYTE pCell,
                             HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.ScrollRt.usTopRow=usTopRow;
+	args.ScrollRt.usLeftCol=usLeftCol;
+	args.ScrollRt.usBotRow=usBotRow;
+	args.ScrollRt.usRightCol=usRightCol;
+	args.ScrollRt.cbCol=cbCol;
+	args.ScrollRt.pCell=pCell;
+	args.ScrollRt.hvio=hvio;
+	
+	return VioCall(FN_SCROLLRT, &args);
 }
 
 
@@ -380,13 +768,23 @@ USHORT APIENTRY VioScrLock (USHORT fWait,
                            PUCHAR pfNotLocked,
                            HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.ScrLock.fWait=fWait;
+	args.ScrLock.pfNotLocked=pfNotLocked;
+	args.ScrLock.hvio=hvio;
+	
+	return VioCall(FN_SCRLOCK, &args);
 }
 
 
 USHORT APIENTRY VioScrUnLock (HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.ScrUnLock.hvio=hvio;
+	
+	return VioCall(FN_SCRUNLOCK, &args);
 }
 
 
@@ -394,7 +792,12 @@ USHORT APIENTRY VioSavRedrawWait (USHORT usRedrawInd,
                                  PUSHORT pNotifyType,
                                  USHORT usReserved)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.SavRedrawWait.usRedrawInd=usRedrawInd;
+	args.SavRedrawWait.pNotifyType=pNotifyType;
+
+	return VioCall(FN_SAVREDRAWWAIT, &args);
 }
 
 
@@ -402,21 +805,36 @@ USHORT APIENTRY VioSavRedrawUndo (USHORT usOwnerInd,
                                  USHORT usKillInd,
                                  USHORT usReserved)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.SavRedrawUndo.usOwnerInd=usOwnerInd;
+	args.SavRedrawUndo.usKillInd=usKillInd;
+	
+	return VioCall(FN_SAVREDRAWUNDO, &args);
 }
 
 
 USHORT APIENTRY VioGetFont (PVIOFONTINFO pviofi,
                            HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.GetFont.pviofi=pviofi;
+	args.GetFont.hvio=hvio;
+	
+	return VioCall(FN_GETFONT, &args);
 }
 
 
 USHORT APIENTRY VioSetFont (PVIOFONTINFO pviofi,
                            HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.SetFont.pviofi=pviofi;
+	args.SetFont.hvio=hvio;
+	
+	return VioCall(FN_SETFONT, &args);
 }
 
 
@@ -424,7 +842,13 @@ USHORT APIENTRY VioGetBuf (PULONG pLVB,
                           PUSHORT pcbLVB,
                           HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.GetBuf.pLVB=pLVB;
+	args.GetBuf.pcbLVB=pcbLVB;
+	args.GetBuf.hvio=hvio;
+	
+	return VioCall(FN_GETBUF, &args);
 }
 
 
@@ -432,7 +856,12 @@ USHORT APIENTRY VioModeWait (USHORT usReqType,
                             PUSHORT pNotifyType,
                             USHORT usReserved)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.ModeWait.usReqType=usReqType;
+	args.ModeWait.pNotifyType=pNotifyType;
+	
+	return VioCall(FN_MODEWAIT, &args);
 }
 
 
@@ -440,7 +869,12 @@ USHORT APIENTRY VioModeUndo (USHORT usOwnerInd,
                               USHORT usKillInd,
                               USHORT usReserved)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.ModeUndo.usOwnerInd=usOwnerInd;
+	args.ModeUndo.usKillInd=usKillInd;
+	
+	return VioCall(FN_MODEUNDO, &args);
 }
 
 
@@ -448,25 +882,23 @@ USHORT APIENTRY VioShowBuf (USHORT offLVB,
                              USHORT cb,
                              HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
-}
-
-
-USHORT APIENTRY VioScrollLf (USHORT usTopRow,
-                            USHORT usLeftCol,
-                            USHORT usBotRow,
-                            USHORT usRightCol,
-                            USHORT cbCol,
-                            PBYTE pCell,
-                            HVIO hvio)
-{
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.ShowBuf.offLVB=offLVB;
+	args.ShowBuf.cb=cb;
+	args.ShowBuf.hvio=hvio;
+	
+	return VioCall(FN_SHOWBUF, &args);
 }
 
 
 USHORT APIENTRY VioPrtScToggle (HVIO hvio)
 {
-  return unimplemented(__FUNCTION__);
+	ARGS args;
+	
+	args.PrtScToggle.hvio=hvio;
+	
+	return VioCall(FN_PRTSCTOGGLE, &args);
 }
 
 
