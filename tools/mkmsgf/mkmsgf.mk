c54bc7b2dca4a2af8c935df96105920ae465445d
valery <_valerius@mail.ru>
2024-06-15 04:05:38 +0000

    Misc Linux fixes.

--- tools/mkmsgf/mkmsgf.mk
+++ tools/mkmsgf/mkmsgf.mk
@@ -4,5 +4,6 @@ PORT_NAME    = tools$(SEP)mkmsgf
 PORT_TYPE    = git
 PORT_URL     = https://github.com/MikeyG/mkmsgf
 PORT_REV     = 1.1
-PORT_PATCHES = mkmsgf.diff mkmsgf2.diff mkmsgf3.diff mkmsgf4.diff mkmsgf5.diff mkmsgf6.diff
+PORT_PATCHES = mkmsgf.diff mkmsgf2.diff mkmsgf3.diff mkmsgf4.diff &
+    mkmsgf5.diff mkmsgf6.diff mkmsgf7.diff
 !include $(%ROOT)tools/mk/port.mk
