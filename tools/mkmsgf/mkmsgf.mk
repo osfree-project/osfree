89a553e151c36032da9aa1cece88b94cf260380d
prokushev <yuri.prokushev@gmail.com>
2024-06-19 09:37:03 +0700

    C style labels now generated for all names

--- tools/mkmsgf/mkmsgf.mk
+++ tools/mkmsgf/mkmsgf.mk
@@ -5,5 +5,5 @@ PORT_TYPE    = git
 PORT_URL     = https://github.com/MikeyG/mkmsgf
 PORT_REV     = 1.1
 PORT_PATCHES = mkmsgf.diff mkmsgf2.diff mkmsgf3.diff mkmsgf4.diff &
-    mkmsgf5.diff mkmsgf6.diff mkmsgf7.diff mkmsgf8.diff
+    mkmsgf5.diff mkmsgf6.diff mkmsgf7.diff mkmsgf8.diff mkmsgf9.diff
 !include $(%ROOT)tools/mk/port.mk
