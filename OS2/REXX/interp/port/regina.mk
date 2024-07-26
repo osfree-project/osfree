6afd284403c390f9ed232b31dea05c114caeb09c
prokushev <yuri.prokushev@gmail.com>
2024-07-26 10:19:10 +0700

    Added support for //t switch for regina

--- OS2/REXX/interp/port/regina.mk
+++ OS2/REXX/interp/port/regina.mk
@@ -4,6 +4,6 @@ PORT_NAME = os2$(SEP)regina
 PORT_TYPE = svn
 PORT_URL  = https://svn.code.sf.net/p/regina-rexx/code/interpreter/
 PORT_REV  = tags/3.9.6/
-PORT_PATCHES  = regina.diff regina2.diff
+PORT_PATCHES  = regina.diff regina2.diff regina3.diff
 
 !include $(%ROOT)tools/mk/port.mk
