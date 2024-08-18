6aa7ee495e75a4dd5a1480fe32014d65dcc254a7
prokushev <yuri.prokushev@gmail.com>
2024-08-18 19:02:00 +0700

    jwasm upgraded to 2.18

--- tools/jwasm/jwasm.mk
+++ tools/jwasm/jwasm.mk
@@ -1,8 +1,6 @@
-# $Id: makefile,v 1.1 2004/08/16 06:27:05 prokushev Exp $
-
 PORT_NAME = tools$(SEP)jwasm
 PORT_TYPE = git
 PORT_URL  = https://github.com/Baron-von-Riedesel/JWasm
-PORT_REV  = v2.17
+PORT_REV  = v2.18
 
 !include $(%ROOT)tools/mk/port.mk
