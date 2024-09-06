866f42be3ff2f971f9552a534740b99313f6e11d
valery <_valerius@mail.ru>
2024-09-06 20:30:35 +0000

    SOM: Return back some lost changes.

--- SOM/port/somfree.mk
+++ SOM/port/somfree.mk
@@ -3,6 +3,6 @@ PORT_NAME = tools$(SEP)somfree
 PORT_TYPE = svn
 PORT_URL  = https://svn.code.sf.net/p/somfree/code/
 PORT_REV  = trunk
-PORT_PATCHES  = somfree.diff somfree2.diff somfree3.diff somfree4.diff somfree5.diff #somfree6.diff
+PORT_PATCHES  = somfree.diff somfree2.diff somfree3.diff somfree4.diff somfree5.diff somfree6.diff
 
 !include $(%ROOT)tools/mk/port.mk
