5a0474b1cc7d847e092fcbedd2edfd1f86a6856e
prokushev <yuri.prokushev@gmail.com>
2024-09-18 08:53:46 +0700

    makefile exports and some shl functions

--- OS2/WPS/PMWP/shl.c
+++ OS2/WPS/PMWP/shl.c
@@ -1,18 +1,58 @@
 #define INCL_PMWP
 #include <os2.h>
 
+typedef struct {
+   EXCEPTIONREGISTRATIONRECORD Exc;
+   ULONG rEBP;
+   ULONG rEBX;
+   ULONG rEDI;
+   ULONG rESI;
+   ULONG rESP;
+   ULONG ret;
+} SHLEXCEPTIONREGISTRATIONRECORD, *PSHLEXCEPTIONREGISTRATIONRECORD;
+
+
 APIRET APIENTRY ShlSaveEnv(PEXCEPTIONREGISTRATIONRECORD pERegRec, PVOID handler)
 {
+	return 0;
+}
+
+#define xEBX offsetof(SHLEXCEPTIONREGISTRATIONRECORD, rEBX)
+
+APIRET APIENTRY ShlRestoreEnv(PEXCEPTIONREGISTRATIONRECORD pERegRec)
+{
+	__asm {
+		mov ecx, pERegRec
+		mov ebx, [ecx+xEBX]
+	}
+	return 0;
 }
 
 APIRET APIENTRY ShlStartWorkplace(HAB hab, HMQ hmq)
 {
+	return 0;
 }
 
 APIRET APIENTRY MessageLoopProc(HAB hab)
 {
+  QMSG   qmsg;
+  HWND   hwnd;
+
+  for (;;)
+  {
+    while (WinGetMsg(hab, &qmsg, 0, 0, 0))
+      WinDispatchMsg(hab, &qmsg);
+
+    hwnd = (HWND)qmsg.mp2;
+
+    if (((HWND)qmsg.msg == WM_QUIT) && (WinIsWindow(hab, hwnd)))
+      WinSendMsg(hwnd, (ULONG)WM_SYSCOMMAND, (MPARAM)SC_CLOSE, (MPARAM)CMDSRC_MENU);
+  }
+
+  return 0;
 }
 
 APIRET APIENTRY ShlExceptionHandler(void)
 {
+  return 0;
 }
