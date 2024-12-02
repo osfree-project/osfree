80132eaa2beafcb2fd9437d0782b39513cf014c7
prokushev <yuri.prokushev@gmail.com>
2024-12-02 14:28:29 +0700

    fix of output under windows

--- tools/lxlite/lxlite.mk
+++ tools/lxlite/lxlite.mk
@@ -4,6 +4,6 @@ PORT_NAME = tools$(SEP)lxlite
 PORT_TYPE = git
 PORT_URL  = https://github.com/bitwiseworks/lxlite
 PORT_REV  = master
-PORT_PATCHES  = lxlite.diff
+PORT_PATCHES  = lxlite.diff lxlite2.diff
 
 !include $(%ROOT)tools/mk/port.mk
