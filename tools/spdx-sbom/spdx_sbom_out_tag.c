3d388b85a8081f93a38ba3d14583b30ef70cc976
prokushev <yuri.prokushev@gmail.com>
2026-09-13 22:50:58 +0700

    Added <text></text> support to rag-value format

--- tools/spdx-sbom/spdx_sbom_out_tag.c
+++ tools/spdx-sbom/spdx_sbom_out_tag.c
@@ -1,7 +1,35 @@
 /* spdx_sbom_out_tag.c - вывод SBOM в формате Tag/Value (C89) */
 
 #include <stdio.h>
+#include <string.h>
 #include "spdx_sbom_out.h"
+
+/* ¬ыводит поле в формате <Tag>: <value>.
+ *
+ * SPDX 2.3, раздел про tag-value:
+ *   "When a field value contains a line break, it must be wrapped in
+ *    <text></text> tags."
+ *
+ * ѕоэтому если значение содержит '\n' или '\r' Ч оборачиваем в <text>
+ * и обрезаем хвостовые переводы строк, чтобы </text> шЄл сразу после
+ * последней содержательной строки. */
+static void print_text_field(const char *tag, const char *value) {
+    const char *end;
+    size_t len;
+
+    if (!value) value = "";
+    if (strchr(value, '\n') == NULL && strchr(value, '\r') == NULL) {
+        printf("%s: %s\n", tag, value);
+        return;
+    }
+    len = strlen(value);
+    end = value + len;
+    while (end > value && (end[-1] == '\n' || end[-1] == '\r')) end--;
+
+    printf("%s: <text>", tag);
+    fwrite(value, 1, (size_t)(end - value), stdout);
+    printf("</text>\n");
+}
 
 int sbom_output_tagvalue(const SpdxDocument *doc) {
     int i;
@@ -26,8 +54,10 @@ int sbom_output_tagvalue(const SpdxDocument *doc) {
     for (i = 0; i < doc->extracted_licenses.count; i++) {
         const ExtractedLicenseInfo *e = &doc->extracted_licenses.items[i];
         printf("LicenseID: %s\n", e->license_id);
-        printf("ExtractedText: %s\n", e->extracted_text ? e->extracted_text : "");
-        printf("LicenseName: %s\n", (e->name && e->name[0]) ? e->name : "NOASSERTION");
+        print_text_field("ExtractedText",
+                         e->extracted_text ? e->extracted_text : "");
+        printf("LicenseName: %s\n",
+               (e->name && e->name[0]) ? e->name : "NOASSERTION");
         printf("LicenseComment: %s\n",
                (e->comment && e->comment[0]) ? e->comment : "NOASSERTION");
         printf("\n");
@@ -37,12 +67,16 @@ int sbom_output_tagvalue(const SpdxDocument *doc) {
     printf("PackageName: %s\n", pkg->name);
     printf("SPDXID: %s\n", pkg->spdx_id);
     printf("PackageDownloadLocation: NOASSERTION\n");
-    if (pkg->version[0]) printf("PackageVersion: %s\n", pkg->version);
-    if (pkg->supplier[0]) printf("PackageSupplier: %s\n", pkg->supplier);
+    if (pkg->version[0])
+        printf("PackageVersion: %s\n", pkg->version);
+    if (pkg->supplier[0])
+        printf("PackageSupplier: %s\n", pkg->supplier);
     printf("PackageLicenseConcluded: %s\n", pkg->license);
     printf("PackageLicenseDeclared: %s\n", pkg->license);
-    if (pkg->copyright[0]) printf("PackageCopyrightText: %s\n", pkg->copyright);
-    if (pkg->purpose[0]) printf("PackagePrimaryPurpose: %s\n", pkg->purpose);
+    if (pkg->copyright[0])
+        print_text_field("PackageCopyrightText", pkg->copyright);
+    if (pkg->purpose[0])
+        printf("PackagePrimaryPurpose: %s\n", pkg->purpose);
     if (pkg->files_analyzed) {
         printf("FilesAnalyzed: true\n");
         printf("PackageVerificationCode: %s\n", pkg->verification_code);
@@ -59,7 +93,8 @@ int sbom_output_tagvalue(const SpdxDocument *doc) {
         printf("LicenseConcluded: %s\n", doc->files.items[i].license);
         printf("LicenseInfoInFile: %s\n", doc->files.items[i].license);
         if (doc->files.items[i].copyright[0])
-            printf("FileCopyrightText: %s\n", doc->files.items[i].copyright);
+            print_text_field("FileCopyrightText",
+                             doc->files.items[i].copyright);
         printf("\n");
     }
 
