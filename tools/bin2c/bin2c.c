a4c77fbc741a39527eaa69b25e90b97022444ea4
prokushev <yuri.prokushev@gmail.com>
2026-09-25 10:01:44 +0700

    doxygen comments

--- tools/bin2c/bin2c.c
+++ tools/bin2c/bin2c.c
@@ -1,3 +1,17 @@
+/*!
+ *  @file bin2c.c
+ *  @brief Binary to C unstructured array converter.
+ *
+ *  Reads a binary file and emits a C source fragment declaring an
+ *  unsigned char array whose bytes are the contents of the input
+ *  file. Options control whether non-ASCII bytes are emitted as
+ *  hexadecimal escapes.
+ *
+ *  Target compiler: Micro-C v3.13, Borland C++ v3.1, v4.0.
+ *
+ *  @copyright Copyright (C) 1995 Steffen Kaiser.
+ */
+
 /* $RCSfile: bin2c.c $
    $Locker: ska $       $Name:  $       $State: Exp $
 
@@ -21,18 +35,44 @@
 //#include "yerror.h"
 
 #ifndef lint
+/*!
+ *  @brief RCS identification string.
+ */
 static char const rcsid[] = 
         "$Id: bin2c.c 1.1 1995/08/10 11:38:39 ska Exp ska $";
 #endif
 
-int ascii = 0;          /* force ASCII output */
+int ascii = 0;          /*!< Non-zero to force ASCII (escaped) output. */
 
+/*!
+ *  @brief Chooses the printf format for one byte.
+ *
+ *  Quotes and backslashes are emitted as escaped literals. Control
+ *  bytes, 0xFF and, when @ref ascii is set, any non-ASCII byte are
+ *  emitted as hexadecimal escapes. Everything else is emitted
+ *  verbatim.
+ *
+ *  @param[in] c Byte to classify.
+ *
+ *  @return printf format string to use for @p c.
+ */
 char *chooseLine(unsigned char c)
 {       return (c == '\'' || c == '\\')? "%s'\\%c'":
                    (iscntrl(c) || c == '\xff'
                         || (!isascii(c) && ascii))? "%s'\\x%x'": "%s'%c'";
 }
 
+/*!
+ *  @brief Reads a binary file and writes a C array declaration.
+ *
+ *  Usage: bin2c [-A] name file
+ *
+ *  @param[in] argc Argument count.
+ *  @param[in] argv Argument vector.
+ *
+ *  @return Exit status.
+ *  @retval 0 Successful completion or a usage error was reported.
+ */
 int main(int argc, char **argv)
 {       int c, i;
         FILE *fin;
